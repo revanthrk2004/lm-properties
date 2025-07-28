d298aca4e58dc746640f6003f4250c5f8435abad
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-07-28 04:32:42 +0100

    linares

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById('menu-toggle').addEventListener('click', () => {
-  document.querySelector('.nav').classList.toggle('show');
+  document.getElementById('mobile-slideout').classList.toggle('show');
+});
+
+document.querySelectorAll('#mobile-slideout .nav a').forEach(link => {
+  link.addEventListener('click', () => {
+    document.getElementById('mobile-slideout').classList.remove('show');
+  });
 });
